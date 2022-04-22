c8888486d1f89af16838ecd1e3e8afc53f7a8c04
drac94 <info@luisguerrero.me>
2022-04-22 18:12:36 -0500

    add vite-plugin-eslint to show eslint erros in the browser overlay

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), checker({ typescript: true })],
+  plugins: [react(), checker({ typescript: true }), eslintPlugin()],
   // @ts-ignore
   test: {
     globals: true,
