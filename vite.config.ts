fe6f2b143e283ddf0ac286da0d1e33f0de5b600d
drac94 <info@luisguerrero.me>
2022-04-22 14:40:00 -0500

    add vite-plugin-checker to show TS errors in the browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true })],
   // @ts-ignore
   test: {
     globals: true,
